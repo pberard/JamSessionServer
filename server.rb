e8afa8af3a4681676ac0b853fbf22cbb0f690f64
Patrick Berard <pberard@carthage.edu>
2014-05-05 12:05:38 -0500

    the id parameters had to be passed in as strings, now I must convert back to ints

--- server.rb
+++ server.rb
@@ -204,8 +204,8 @@ get '/getUpdates' do
 	logger.info "Get Updates!"
 	jsonHash = {}
 	logger.info "Params: " + params[:userID]
-	logger.info "SQL:::" + Jam.where(:id => Collaboration.select(:jam_id).where(:user_id => params[:userID])).exclude(:id => Song.select(:jam_id).where(:user_id => params[:userID])).sql
-	jams = Jam.where(:id => Collaboration.select(:jam_id).where(:user_id => params[:userID])).exclude(:id => Song.select(:jam_id).where(:user_id => params[:userID]))
+	logger.info "SQL:::" + Jam.where(:id => Collaboration.select(:jam_id).where(:user_id => params[:userID].to_i)).exclude(:id => Song.select(:jam_id).where(:user_id => params[:userID].to_i)).sql
+	jams = Jam.where(:id => Collaboration.select(:jam_id).where(:user_id => params[:userID].to_i)).exclude(:id => Song.select(:jam_id).where(:user_id => params[:userID].to_i))
 	jams.each{ |jam|
 		logger.info "Jam: " + jam.to_s
 		jamHash = {:id => jam[:id],
